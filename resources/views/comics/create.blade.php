16c951c9ec5c130d33861c3e1d682fde41af7a7d
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-06-14 21:05:23 +0200

    feat: added form for insertion of new comic

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -1,5 +1,49 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>creation page</h1>
+    <div class="container">
+        <h1>Add a new comic</h1>
+
+        <form action="#" method="POST">
+            @csrf
+
+            <div class="mb-3">
+                <label for="title" class="form-label">Titolo</label>
+                <input type="text" class="form-control" id="title" name="title">
+            </div>
+            
+            <div class="mb-3">
+                <label for="description" class="form-label">Descrizione</label>
+                <textarea class="form-control" id="description" name="description" rows="3"></textarea>
+            </div>
+            
+            <div class="mb-3">
+                <label for="thumb" class="form-label">Tempo di cottura</label>
+                <input type="text" class="form-control" id="thumb" name="thumb">
+            </div>
+
+            <div class="mb-3">
+                <label for="price" class="form-label">Peso</label>
+                <input type="text" class="form-control" id="price" name="price">
+            </div>
+
+            <div class="mb-3">
+                <label for="series" class="form-label">Image</label>
+                <input type="text" class="form-control" id="series" name="series">
+            </div>
+
+            <div class="mb-3">
+                <label for="sale_date">Start date:</label>
+                <input type="date" class="form-control" id="sale_date" name="sale_date"/>
+            </div>
+
+            <div class="mb-3">
+                <label for="type" class="form-label">Image</label>
+                <input type="text" class="form-control" id="type" name="type">
+            </div>
+
+
+            <button class="btn btn-success" type="submit">Salva</button>
+        </form>
+    </div>
 @endsection

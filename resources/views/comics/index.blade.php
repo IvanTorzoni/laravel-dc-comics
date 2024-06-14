ea9a4f58fb819a17adfd9a07c41b8eee2fbecc77
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-06-14 17:11:06 +0200

    feat: printed card in page

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,9 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Comics Page</h1>
-
-    @foreach ($comics as $comic)
-        <p> {{ $comic->title}}</p>
-    @endforeach
+    <section>
+        <div class="container text-center">
+            <h1 class="py-5">Comics page</h1>
+            <div class="row g-3">
+                @foreach ($comics as $comic)
+                    <div class="col-12 col-md-6 col-lg-4 mb-4">
+                        <div class="card" style="width: 18rem;">
+                            <img src=" {{ $comic->thumb }} " class="img-fluid" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title"> {{ $comic->title }} </h5>
+                                <a href="#" class="btn btn-primary">Comic Details</a>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </section>
 @endsection

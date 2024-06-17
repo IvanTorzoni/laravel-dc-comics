848ced6ce06f7af9010feae7837b5ee3981d3037
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-06-17 10:55:49 +0200

    feat: added edit page

---
+++ resources/views/comics/edit.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+
+        <h1 class="py-5">Pagina di edit</h1>
+
+        <p> {{ $comic->title }}</p>
+    </div>
+@endsection
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -12,6 +12,7 @@
                             <div class="card-body">
                                 <h5 class="card-title"> {{ $comic->title }} </h5>
                                 <a href="{{route('comics.show', ['comic' => $comic->id])}}" class="btn btn-primary">Comic Details</a>
+                                <a href="{{route('comics.edit', ['comic' => $comic->id])}}" class="btn btn-warning">Comic Edit</a>
                             </div>
                         </div>
                     </div>

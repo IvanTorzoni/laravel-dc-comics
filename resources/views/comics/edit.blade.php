572134085bd2811f4e567590719de2f203b89892
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-06-17 12:19:04 +0200

    feat: creation of edit form

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -3,8 +3,49 @@
 @section('content')
     <div class="container">
 
-        <h1 class="py-5">Pagina di edit</h1>
+        <h1 class="py-5 text-center">Pagina di edit</h1>
 
-        <p> {{ $comic->title }}</p>
+        <form action="{{ route('comics.update', ['comic' => $comic->id])}}" method="POST>
+            @csrf
+            @method('PUT')
+
+            <div class="mb-3">
+                <label for="title" class="form-label">Title</label>
+                <input type="text" class="form-control" id="title" name="title" value="{{ $comic->title }}">
+            </div>
+            
+            <div class="mb-3">
+                <label for="description" class="form-label">Description</label>
+                <textarea class="form-control" id="description" name="description" rows="3"> {{ $comic->description }} </textarea>
+            </div>
+            
+            <div class="mb-3">
+                <label for="thumb" class="form-label">Image</label>
+                <input type="text" class="form-control" id="thumb" name="thumb" value="{{ $comic->thumb }}">
+            </div>
+
+            <div class="mb-3">
+                <label for="price" class="form-label">Price</label>
+                <input type="text" class="form-control" id="price" name="price" value="{{ $comic->price }}">
+            </div>
+
+            <div class="mb-3">
+                <label for="series" class="form-label">Series</label>
+                <input type="text" class="form-control" id="series" name="series" value="{{ $comic->series }}">
+            </div>
+
+            <div class="mb-3">
+                <label for="sale_date">Sale date</label>
+                <input type="date" class="form-control" id="sale_date" value="{{ $comic->sale_date }}" name="sale_date"/>
+            </div>
+
+            <div class="mb-3">
+                <label for="type" class="form-label">Type</label>
+                <input type="text" class="form-control" id="type" name="type" value="{{ $comic->type }}">
+            </div>
+
+
+            <button class="btn btn-success" type="submit">Salva</button>
+        </form>
     </div>
 @endsection
